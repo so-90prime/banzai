dc5bec330a4f0bdf258007b5c8dbdf3f6d66d04f
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2022-11-30 18:45:24 -0700

    add first draft of focus test

--- banzai/qc/__init__.py
+++ banzai/qc/__init__.py
@@ -3,6 +3,7 @@ from banzai.qc.pointing import PointingTest
 from banzai.qc.sinistro_1000s import ThousandsTest
 from banzai.qc.pattern_noise import PatternNoiseDetector
 from banzai.qc.header_checker import HeaderSanity
+from banzai.qc.focus import FocusTest
 
 __all__ = ['SaturationTest', 'PointingTest', 'ThousandsTest',
-           'PatternNoiseDetector', 'HeaderSanity']
+           'PatternNoiseDetector', 'HeaderSanity', 'FocusTest']
