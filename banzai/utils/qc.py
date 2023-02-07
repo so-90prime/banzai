d7a5cb303b1a7f28a9cb6165d13a2d84deba2731
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2023-02-07 00:13:34 -0700

    handle NaN in logs

--- banzai/utils/qc.py
+++ banzai/utils/qc.py
@@ -20,9 +20,11 @@ def save_qc_results(runtime_context, qc_results, image):
     File name, site, instrument ID, dateobs, obstype, and timestamp are always saved in the database.
     """
     for key, value in qc_results.items():
-        # JSON fields do not like numpy.bool_ types
+        # JSON fields do not like numpy.bool_ types or NaNs
         if type(value) == np.bool_:
             value = bool(value)
+        elif isinstance(value, float) and np.isnan(value):
+            value = 'NaN'
         qc_results[key] = value
     with dbs.get_session(db_address=runtime_context.DB_ADDRESS) as db_session:
         record = dbs.Log(filename=image.filename.replace('.fits', '').replace('.fz', ''),
